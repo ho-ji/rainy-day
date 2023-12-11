29c1977c907d513639a864d475fca333acda41d7
ho-ji <wkdd0205@gmail.com>
2023-12-11 18:35:09 +0900

    feat: 현재 위치 카카오맵 마커 추가

---
+++ rainy-day/src/components/CurrentLocationWeather.vue
@@ -0,0 +1,87 @@
+<template lang="">
+  <section>
+    <h2>현재위치</h2>
+    <div id="info">
+      <table>
+        weather info
+      </table>
+      <div
+        ref="map"
+        id="map"></div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'CurrentLocationWeather',
+
+  data() {
+    return {
+      map: null,
+      markers: [],
+    }
+  },
+  props: {
+    latitude: Number,
+    longitude: Number,
+  },
+  mounted() {
+    if (!this.latitude || !this.longitude) return null
+    if (window.kakao && window.kakao.maps) {
+      this.initMap()
+    } else {
+      const script = document.createElement('script')
+      script.src = '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=15c0e35831cafc8c9883fd79e26612d8'
+      document.head.appendChild(script)
+      script.onload = () => window.kakao.maps.load(this.initMap)
+    }
+  },
+  methods: {
+    initMap() {
+      const container = document.getElementById('map')
+      const options = {
+        center: new window.kakao.maps.LatLng(this.latitude, this.longitude),
+        level: 5,
+      }
+      this.map = new window.kakao.maps.Map(container, options)
+      this.displayMarker([[this.latitude, this.longitude]])
+    },
+    displayMarker(markerPositions) {
+      if (this.markers.length > 0) {
+        this.markers.forEach((marker) => marker.setMap(null))
+      }
+
+      const positions = markerPositions.map((position) => new window.kakao.maps.LatLng(...position))
+
+      if (positions.length > 0) {
+        this.markers = positions.map(
+          (position) =>
+            new window.kakao.maps.Marker({
+              map: this.map,
+              position,
+            })
+        )
+
+        const bounds = positions.reduce((bounds, latlng) => bounds.extend(latlng), new window.kakao.maps.LatLngBounds())
+
+        this.map.setBounds(bounds)
+      }
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+section {
+  width: 100rem;
+  height: 50rem;
+  background-color: red;
+}
+#info {
+  display: flex;
+  justify-content: space-between;
+}
+#map {
+  width: 50rem;
+  height: 50rem;
+}
+</style>
--- rainy-day/src/pages/Home.vue
+++ rainy-day/src/pages/Home.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="wrap">
-    <div class="container">hi</div>
-  </div>
+  <h1>여기에 비가 오나요?</h1>
+  <main>
+    <CurrentLocationWeather
+      v-for="l in location"
+      :key="l"
+      :latitude="l.latitude"
+      :longitude="l.longitude" />
+    <button type="button">장소 추가</button>
+  </main>
 </template>
 <script>
-export default {}
+import CurrentLocationWeather from '@/components/CurrentLocationWeather.vue'
+export default {
+  name: 'Home',
+  components: {
+    CurrentLocationWeather,
+  },
+  data() {
+    return {
+      location: [],
+    }
+  },
+  created() {
+    if (!('geolocation' in navigator)) {
+      return
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+      this.location.push({latitude, longitude})
+    })
+  },
+}
 </script>
 <style lang="scss" scoped>
-@import '@/scss/main.scss';
+h1 {
+  margin-top: 5rem;
+  text-align: center;
+  font-size: 3.2rem;
+}
+main {
+  margin: 0 10rem;
+}
 </style>
