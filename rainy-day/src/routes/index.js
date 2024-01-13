abb135f5fbbbcbcd5e3f05305da0d0f09acd7048
ho-ji <wkdd0205@gmail.com>
2024-01-14 02:07:06 +0900

    feat: 해쉬모드 삭제 및 / 경로 아닐 시 리다이렉트 추가

--- rainy-day/src/routes/index.js
+++ rainy-day/src/routes/index.js
@@ -1,14 +1,18 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../pages/Home'
 
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
 
   routes: [
     {
       path: '/',
       name: Home,
       component: Home,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/',
     },
   ],
 })
